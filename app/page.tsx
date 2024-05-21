9f0539bedf19249f1bf84e5a31bc61cbe3c1e07a
BRp99 <124196319+BRp99@users.noreply.github.com>
2024-05-21 21:42:33 +0100

    remove 'KIKI' component inside Home.tsx to resolve extra rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 import CryptoPriceList from "../components/CryptoPriceList"
-import KIKI from "@/components/TokenCard"
 
 export default function Home() {
   return (
@@ -8,7 +7,6 @@ export default function Home() {
       <h1 className="text-7xl font-poppins font-bold text-emerald-50 ">ChainLink Cripto Pulse</h1>
       <h2 className="text-3xl font-poppins font-bold text-emerald-100 ">ERC20 tokens price</h2>
       <CryptoPriceList />
-      <KIKI />
     </div>
   )
 }
