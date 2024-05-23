9183b0a274e84439a59f24ab7db397c1803c291e
BRp99 <124196319+BRp99@users.noreply.github.com>
2024-05-23 10:02:28 +0100

    unistall '@next/font' and install 'next/font' to version 14 + install nextui-org/skeleton

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next"
-import { Barlow } from "@next/font/google"
+import { Barlow } from "next/font/google"
 import "./globals.css"
 import { NextUIProvider } from "@nextui-org/react"
 
